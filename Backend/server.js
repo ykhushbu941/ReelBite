1d66405a1b329e68924f953cc74995d39fe9c765
Khushbu Yadav <your-email@example.com>
2026-01-29 21:20:57 +0530

    database

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 // start server
 const app = require('./src/app');
+const connectDB = require('./src/db/db');
 
+connectDB();
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
