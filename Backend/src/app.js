699d999dda2c31c6121b73cdd2755593ff34b6a9
Khushbu Yadav <your-email@example.com>
2026-02-20 11:34:04 +0530

    app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 // create server
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 app.use(cookieParser());
@@ -10,5 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.setEncoding("Hello World");
 })
+
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
