ca2002d93aa734a3787fb408a0ff38027412d840
Khushbu Yadav <your-email@example.com>
2026-03-15 20:49:58 +0530

    auth

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -8,3 +8,4 @@ router.post('/user/register', authController.registerUser)
 router.post('/user/login', authController.loginUser)
 router.get('/user/logout', authController.logoutUser)
 
+module.exports = router;
