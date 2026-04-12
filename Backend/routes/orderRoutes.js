c06e634d58cabcacdaadacfaf80b82882faf38f1
Khushbu Yadav <your-email@example.com>
2026-04-12 10:22:12 +0530

    orders

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -10,4 +10,3 @@ router.get("/partner", protect, isPartner, getPartnerOrders);
 router.put("/:id/status", protect, isPartner, updateOrderStatus); // Only partner updates status
 router.put("/:id/cancel", protect, cancelOrder); // User can cancel
 
-module.exports = router;
