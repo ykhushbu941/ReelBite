360d46849fe6cb192791a9f7e5815fd04e21b90b
Khushbu Yadav <your-email@example.com>
2026-07-08 16:08:58 +0530

    deploy

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,10 @@ const errorHandler = require("./middleware/errorMiddleware");
 const app = express();
 
 // 🔧 Middlewares
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -42,7 +45,7 @@ app.use(errorHandler);
 // Serve frontend in production
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "deploy") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
   });
 }
